a63b4d2b8166d247e3637e7d2a9f8bbf8520a8f9
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-05-14 17:30:01 +0200

    fixed the study drills, one was hard

--- ArrayLinearSearch.java
+++ ArrayLinearSearch.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
 
+/*
+*	Study Drill:
+*	1. Declaring the int num before the for loops
+*	did not work. 
+*	2. The study drill was hard, and it took some research
+* 	to figure out. I think there are actually many
+*	ways to actually do this, but I'm not
+*	good enough yet.
+*/
+
 public class ArrayLinearSearch {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		int[] orderNumbers = {12345, 54321, 101010,
 			8675309, 31415, 271828};
+		//int num;
 		int toFind;
-
+		boolean found = false;
 		System.out.print("There are " + orderNumbers.length);
 		System.out.println(" orders in the database.");
 
@@ -22,11 +33,11 @@ public class ArrayLinearSearch {
 		for (int num: orderNumbers) {
 			if (num == toFind) {
 				System.out.println(num + " found");
-			} else {
-				System.out.println("Sorry, " + toFind + 
-					" not found.");
-			}
-			return;
+				found = true;
+			} 
+		} 
+		if (found == false) {
+			System.out.println("Sorry " + toFind + " doesn't exist.");
 		}
-	}
+	}	
 }
